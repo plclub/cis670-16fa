39684f3d9dd713e8da07b1d99200f5a4f042d74a
Stephanie Weirich <sweirich@cis.upenn.edu>
2016-10-18 20:52:06 -0400

    small examples of systemf

---
+++ code/systemf_example.v
@@ -0,0 +1,32 @@
+Add LoadPath "/Users/sweirich/github/deepspec/metalib/" as Metalib.
+Require Import Metalib.Metatheory.
+
+Require Import systemf_ott.
+
+Definition void := t_all (t_var_b 0).
+
+Definition unit := t_all (t_arr (t_var_b 0) (t_var_b 0)).
+Definition tt   := e_Lam (e_lam (t_var_b 0) (e_var_b 0)).
+
+Lemma lc_Typ_void : lc_Typ void.
+Proof.
+  eapply lc_t_all. intro X.
+  unfold open_Typ_wrt_Typ. simpl.
+  eapply lc_t_var_f.
+Qed.
+
+Lemma tt_unit : exp d_empty g_empty tt unit.
+Proof.
+  unfold unit. unfold tt.
+  pick fresh X and apply exp_Lam.
+  unfold open_Exp_wrt_Typ. unfold open_Typ_wrt_Typ.
+  simpl.
+  pick fresh x and apply exp_lam.
+  auto.
+  unfold open_Exp_wrt_Exp. simpl.
+  eapply exp_var.
+  auto.
+  auto.
+  auto.
+  auto.
+Qed.
