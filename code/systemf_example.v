c34c2467abddb9c7f0de204b6f01acbcbc6cb79f
Stephanie Weirich <sweirich@cis.upenn.edu>
2016-10-19 14:07:38 -0400

    filled out pair example

--- code/systemf_example.v
+++ code/systemf_example.v
@@ -1,3 +1,4 @@
+Add LoadPath "/Users/sweirich/github/deepspec/metalib/" as Metalib.
 Require Import Metalib.Metatheory.
 
 Require Import systemf_ott.
@@ -30,9 +31,57 @@ Proof.
   auto.
 Qed.
 
+
+
 Definition prod (t1 t2 : Typ) : Typ :=
   t_all (t_arr (t_arr t1 (t_arr t2 (t_var_b 0))) (t_var_b 0)).
 
+Definition pair : Exp :=
+  e_Lam
+    (e_Lam
+       (e_lam (t_var_b 1)
+          (e_lam (t_var_b 0)
+                 (e_Lam
+                    (e_lam (t_arr (t_var_b 2) (t_arr (t_var_b 1) (t_var_b 0)))
+                           (e_ap (e_ap (e_var_b 0) (e_var_b 2)) (e_var_b 1))))))).
+
+Lemma pair_typing : 
+  exp d_empty g_empty pair (t_all (t_all (t_arr (t_var_b 1)
+                                                (t_arr (t_var_b 0)
+                                             (prod (t_var_b 2) (t_var_b 1)))))).
+Proof.
+  intros.
+  unfold pair, prod.
+  pick fresh X and apply exp_Lam.
+  unfold open_Exp_wrt_Typ, open_Typ_wrt_Typ. simpl.
+  pick fresh Y and apply exp_Lam.
+  unfold open_Exp_wrt_Typ, open_Typ_wrt_Typ. simpl.
+
+  pick fresh x and apply exp_lam.
+  admit. (* type X *)
+  unfold open_Exp_wrt_Exp. simpl.
+
+  pick fresh y and apply exp_lam.
+  admit. (* type Y *)
+  unfold open_Exp_wrt_Exp. simpl.
+
+  pick fresh Z and apply exp_Lam.
+  unfold open_Exp_wrt_Typ, open_Typ_wrt_Typ. simpl.
+
+  pick fresh z and apply exp_lam.
+  eapply type_arr.  admit. (* type X *)
+  eapply type_arr.  admit. (* type Y *)
+  eapply type_var. auto. auto.
+  
+  unfold open_Exp_wrt_Exp. simpl.
+  eapply exp_ap; auto.
+  eapply exp_ap; eauto.
+  eapply exp_var; eauto.
+
+  admit. (* x has type X *)
+  admit. (* y has type Y *)
+Admitted.
+  
 Definition sum (t1 t2 : Typ) : Typ :=
   t_all (t_arr (t_arr t1 (t_var_b 0)) (t_arr (t_arr t2 (t_var_b 0)) (t_var_b 0))).
 
